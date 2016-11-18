e3e5191ad7f86b33d78b3dc5fcb2564e7ece68ed
JEAN-CHARLES Loïc ( freshloic) <loicjeancharles@gmail.com>
2016-11-18 14:04:41 -0400

    css 2

--- index.php
+++ index.php
@@ -17,16 +17,21 @@
       switch($_SESSION["page"]){
 	 case "accueil":
 ?>
-  <h1>Accueil</h1>
-  <form method="POST" action="controleur.php">
+  <form style="float: right;" method="POST" action="controleur.php">
     <input type="hidden" name="etat-f" value="demande connexion"/>
     <input type="submit" value="connexion"/> 
   </form>
 
-  <form method="POST" action="controleur.php">
+  <form style="float: right;" method="POST" action="controleur.php">
    <input type="hidden" name="etat-f" value="demande inscription"/>
    <input type="submit" value="inscription"/>
   </form>
+
+  <h1 style="text-align: center;">Bienvenue sur freshMAG</h1>
+  <h2 style="text-align: center; margin-right: 150px;">xxx Membres</h2>
+  <h2 style="text-align: center; margin-right: 150px;">xxx Relations</h2>
+
+  <img style="float: right;" src="https://unsplash.it/221/252" alt="site picture">
 <?php 
         break;
         case "formulaire connexion":
@@ -38,6 +43,9 @@
     mot de passe: <input type="password" name="password" value=""/><br>
     <input type="submit" value="connexion"/> 
   </form>
+
+  <a href="index.php">S'inscrire</a> 
+
 <?php 
         break;
         case "formulaire inscription":
